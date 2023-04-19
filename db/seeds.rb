e9086125f62dcce9f8eddff321b48819834333ef
Luisa Ruiz <luisa.ruiz@ieseg.fr>
2023-04-19 16:23:42 +0200

    part navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+Product.destroy_all
+User.destroy_all
+
+user = User.create(email: "owner@gmail.com", password: "123456", )
+
+file = URI.open("https://static.zara.net/photos///2023/V/0/1/p/7149/042/800/12/w/750/7149042800_1_1_1.jpg?ts=1681122133175")
+product = Product.create(name: "HIGH-WAIST SHORTS WITH PLEATS", description: "High-waist shorts with belt loops. Front pleats. Front zip fly and top button fastening.", price: "29.95", user:user)
+product.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+product.save
