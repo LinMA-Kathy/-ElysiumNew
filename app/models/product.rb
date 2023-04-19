25c2153c86b1d92796e746185b35828faa245068
Luisa Ruiz <luisa.ruiz@ieseg.fr>
2023-04-19 17:55:10 +0200

    seed

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :transactions, dependent: :destroy
   validates :name, :price, :description, presence: true
-  validates :photo, presence: true
+  has_one_attached :photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,10 @@ require "open-uri"
 Product.destroy_all
 User.destroy_all
 
-user = User.create(email: "owner@gmail.com", password: "123456", )
+user = User.create(email: "owner@gmail.com", password: "123456")
+user.save!
 
-file = URI.open("https://static.zara.net/photos///2023/V/0/1/p/7149/042/800/12/w/750/7149042800_1_1_1.jpg?ts=1681122133175")
-product = Product.create(name: "HIGH-WAIST SHORTS WITH PLEATS", description: "High-waist shorts with belt loops. Front pleats. Front zip fly and top button fastening.", price: "29.95", user:user)
+file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
+product = Product.new(name: "HIGH-WAIST SHORTS WITH PLEATS", description: "High-waist shorts with belt loops. Front pleats. Front zip fly and top button fastening.", price: "29")
 product.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 product.save
