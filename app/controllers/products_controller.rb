2792eef755bdbd35c974df04242e151c6306dc9e
lin MA <lin.ma@ieseg.fr>
2023-04-20 15:22:40 +0200

    seeds and index page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,19 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    if params[:search]
+      @products = product.search(params[:search]).order(created_at: :desc)
+      else
+      @products = Product.all
+    end
   end
 
   def new
     @product = Product.new
+  end
+
+
+  def image_url(image_identifier)
+    api_key = ENV['API_KEY']
+    "https://api.example.com/images/#{image_identifier}?api_key=#{api_key}"
   end
 end
