79926499ba465ff66dea70022ee0f27d9a1ca571
Qian zhou <qian.zhou@ieseg.fr>
2023-04-24 16:40:41 +0200

    homepage5.0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def user_params
   params.require(:user).permit(:first_name, :last_name, :photo)
   end
+
+  def update
+  @user = User.find(params[:id])
+  @user.update(params[:user])
+  redirect_to dashboard_path(@dashboard)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
 end
